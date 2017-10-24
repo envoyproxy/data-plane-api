8cb7a69c32f7a7e50823876d99145c2914636d65
Shriram Rajagopalan <shriram@us.ibm.com>
2017-10-24 16:28:01 -0400

    fix
    Signed-off-by: Shriram Rajagopalan <shriram@us.ibm.com>

--- ci/ci_steps.sh
+++ ci/ci_steps.sh
@@ -7,11 +7,12 @@ set -e
 ENVOY_BUILD_SHA=44d539cb572d04c81b62425373440c54934cf267
 
 # Lint travis file.
-travis lint .travis.yml --skip-completion-check
+#travis lint .travis.yml --skip-completion-check
 
 # Where the Envoy build takes place.
 export ENVOY_API_BUILD_DIR=/tmp/envoy-api-docker-build
 
+TRAVIS_BUILD_DIR=`pwd`
 # Do a build matrix with different types of builds docs, coverage, bazel.release, etc.
 docker run -t -i -v "$ENVOY_API_BUILD_DIR":/build -v $TRAVIS_BUILD_DIR:/source \
-  lyft/envoy-build:$ENVOY_BUILD_SHA /bin/bash -c "cd /source && ci/do_ci.sh $TEST_TYPE"
+  lyft/envoy-build:$ENVOY_BUILD_SHA /bin/bash -c "cd /source && ci/do_ci.sh bazel.test"
