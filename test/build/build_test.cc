55c8bab2e657897ef0a541c783de854b612ad7bc
Matt Klein <mklein@lyft.com>
2017-10-29 19:11:37 -0700

    comments
    Signed-off-by: Matt Klein <mklein@lyft.com>

--- test/build/build_test.cc
+++ test/build/build_test.cc
@@ -6,12 +6,12 @@
 // Basic C++ build/link validation for the v2 xDS APIs.
 int main(int argc, char *argv[]) {
   const auto methods = {
+    "envoy.api.v2.filter.AccessLogService.StreamAccessLogs",
     "envoy.api.v2.AggregatedDiscoveryService.StreamAggregatedResources",
     "envoy.api.v2.ClusterDiscoveryService.FetchClusters",
     "envoy.api.v2.ClusterDiscoveryService.StreamClusters",
     "envoy.api.v2.EndpointDiscoveryService.FetchEndpoints",
     "envoy.api.v2.EndpointDiscoveryService.StreamEndpoints",
-    "envoy.api.v2.EnvoyAccessLogService.StreamAccessLogs",
     "envoy.api.v2.HealthDiscoveryService.FetchHealthCheck",
     "envoy.api.v2.HealthDiscoveryService.StreamHealthCheck",
     "envoy.api.v2.ListenerDiscoveryService.FetchListeners",
